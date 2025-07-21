9a752e8aeef1e4a8b9f6c1c7190f6f9566f8432a
Aliaksei Palanevich <aliakseipalanevich@bayview.com>
2025-07-21 12:44:41 +0300

    feat: setup task-management-service connection to message-broker

--- task-management-service/src/main.ts
+++ task-management-service/src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {brokers: ['broker:29092'], clientId: 'task-management-service'}, // like docker
+      consumer: {
+        groupId: "task-management-consumer-group"
+      }
+    }
+  })
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log('Task management service started');
+});
