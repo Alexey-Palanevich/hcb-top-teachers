c22ec44e8d0242c717a6f86868897addebd76e43
Aliaksei Palanevich <aliakseipalanevich@bayview.com>
2025-07-21 12:44:03 +0300

    feat: switch teacher-hiring-service to fastify adapter

--- teacher-hiring-service/src/main.ts
+++ teacher-hiring-service/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.KAFKA,
@@ -16,6 +17,8 @@ async function bootstrap() {
   })
 
   await app.startAllMicroservices();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log('Teacher hiring service started')
+});
